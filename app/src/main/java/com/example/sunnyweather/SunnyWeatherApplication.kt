3d5cd16aaa201f00def2a06178d6f2853a399eab
lixin <644564080@qq.com>
2020-05-20 15:22:07 +0800

    实现了搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,19 @@
+package com.example.sunnyweather
+
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application(){
+
+
+    companion object{
+        const val TOKEN = "QYMvUcI12BVn6GKX"
+
+        lateinit var context:Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
