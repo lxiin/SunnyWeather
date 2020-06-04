c87ecfe24ce244d6a496278aba30d302719dc634
lixin <644564080@qq.com>
2020-06-04 13:40:03 +0800

    添加了一句注释

--- app/src/main/java/com/example/sunnyweather/logic/model/Sky.kt
+++ app/src/main/java/com/example/sunnyweather/logic/model/Sky.kt
@@ -4,6 +4,9 @@ import com.example.sunnyweather.R
 
 class Sky (val info: String, val icon: Int, val bg: Int)
 
+/**
+ *  图片相关的Map
+ */
 private val sky = mapOf(
     "CLEAR_DAY" to Sky("晴", R.drawable.ic_clear_day, R.drawable.bg_clear_day),
     "CLEAR_NIGHT" to Sky("晴", R.drawable.ic_clear_night, R.drawable.bg_clear_night),
